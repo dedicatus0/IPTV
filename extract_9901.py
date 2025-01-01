904617263525acb0ba1c628ca3a8511cf35914b6
dedicatus0 <124658650+dedicatus0@users.noreply.github.com>
2025-01-01 22:15:19 +0800

    Update extract_9901.py

--- extract_9901.py
+++ extract_9901.py
@@ -5,12 +5,16 @@ url = 'https://raw.githubusercontent.com/yuanzl77/IPTV/refs/heads/main/live.m3u'
 response = requests.get(url)
 lines = response.text.splitlines()
 
-# 提取9901端口的地址和对应的#EXTINF行
+# 提取特定地址和对应的#EXTINF行
 filtered_lines = []
+seen_addresses = set()
 for i in range(len(lines)):
     if 'http://ottrrs.hl.chinamobile.com/' in lines[i]:
-        filtered_lines.append(lines[i-1])
-        filtered_lines.append(lines[i])
+        address = lines[i]
+        if address not in seen_addresses:
+            seen_addresses.add(address)
+            filtered_lines.append(lines[i-1])
+            filtered_lines.append(lines[i])
 
 # 写入新的文件
 with open('live.m3u', 'w') as f:
