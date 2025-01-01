70ee08fb0ca72dce7bf5efb11ef7e5fdb19bd90b
dedicatus0 <124658650+dedicatus0@users.noreply.github.com>
2025-01-01 22:21:12 +0800

    Update extract_9901.py

--- extract_9901.py
+++ extract_9901.py
@@ -1,20 +1,42 @@
 import requests
+import ipaddress
 
 # 下载文件
 url = 'https://raw.githubusercontent.com/yuanzl77/IPTV/refs/heads/main/live.m3u'
 response = requests.get(url)
 lines = response.text.splitlines()
 
-# 提取特定地址和对应的#EXTINF行
+# 中国移动的IP段列表
+cmcc_ip_ranges = [
+    "39.0.0.0/8",
+    "117.136.0.0/13",
+    "221.176.0.0/13",
+    "183.128.0.0/11",
+    "211.136.0.0/13"
+]
+
+def is_cmcc_ip(ip):
+    for ip_range in cmcc_ip_ranges:
+        if ipaddress.ip_address(ip) in ipaddress.ip_network(ip_range):
+            return True
+    return False
+
+# 提取特定地址和对应的#EXTINF行，同时去除IPV6地址
 filtered_lines = []
 seen_addresses = set()
 for i in range(len(lines)):
-    if 'http://ottrrs.hl.chinamobile.com/' in lines[i]:
-        address = lines[i]
-        if address not in seen_addresses:
-            seen_addresses.add(address)
-            filtered_lines.append(lines[i-1])
-            filtered_lines.append(lines[i])
+    if lines[i].startswith("http://"):
+        # 去除IPV6地址
+        try:
+            ip = lines[i].split("//")[1].split("/")[0].split(":")[0]
+            if ":" in ip:  # 如果包含冒号，则为IPV6地址
+                continue
+            if is_cmcc_ip(ip) and lines[i] not in seen_addresses:
+                seen_addresses.add(lines[i])
+                filtered_lines.append(lines[i-1])
+                filtered_lines.append(lines[i])
+        except Exception as e:
+            continue
 
 # 写入新的文件
 with open('live.m3u', 'w') as f:
