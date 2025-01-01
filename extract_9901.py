c6818c0d2d74d9cd5d2babcad9c7bf58f21a0a46
dedicatus0 <124658650+dedicatus0@users.noreply.github.com>
2025-01-01 22:24:56 +0800

    Update extract_9901.py

--- extract_9901.py
+++ extract_9901.py
@@ -6,17 +6,19 @@ url = 'https://raw.githubusercontent.com/yuanzl77/IPTV/refs/heads/main/live.m3u'
 response = requests.get(url)
 lines = response.text.splitlines()
 
-# 中国移动的IP段列表
-cmcc_ip_ranges = [
-    "39.0.0.0/8",
-    "117.136.0.0/13",
-    "221.176.0.0/13",
-    "183.128.0.0/11",
-    "211.136.0.0/13"
+# 中国联通的IP段列表
+cucc_ip_ranges = [
+    "101.0.0.0/12",
+    "106.0.0.0/12",
+    "110.80.0.0/12",
+    "111.64.0.0/12",
+    "113.128.0.0/12",
+    "118.144.0.0/12",
+    "120.64.0.0/12"
 ]
 
-def is_cmcc_ip(ip):
-    for ip_range in cmcc_ip_ranges:
+def is_cucc_ip(ip):
+    for ip_range in cucc_ip_ranges:
         if ipaddress.ip_address(ip) in ipaddress.ip_network(ip_range):
             return True
     return False
@@ -31,7 +33,7 @@ for i in range(len(lines)):
             ip = lines[i].split("//")[1].split("/")[0].split(":")[0]
             if ":" in ip:  # 如果包含冒号，则为IPV6地址
                 continue
-            if is_cmcc_ip(ip) and lines[i] not in seen_addresses:
+            if is_cucc_ip(ip) and lines[i] not in seen_addresses:
                 seen_addresses.add(lines[i])
                 filtered_lines.append(lines[i-1])
                 filtered_lines.append(lines[i])
